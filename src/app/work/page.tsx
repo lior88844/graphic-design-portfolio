8876938e26798d0513ac8efdf5ce60bfca59e31a
lior88844 <lior88844@gmail.com>
2026-06-22 18:12:45 +0300

    changes in phtos

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -88,9 +88,9 @@ const CATEGORIES: Category[] = [
     cta: 'View Gallery',
     color: '#F4A261',
     images: {
-      leftTop: '/images/photography/01.jpg',
-      leftBottom: '/images/photography/03.jpg',
-      right: '/images/photography/05.jpg',
+      leftTop: '/images/photography/10.jpg',
+      leftBottom: '/images/photography/22.jpg',
+      right: '/images/photography/15.jpg',
     },
   },
 ];
